3a5f42b5fcc65198fec242dda8c6b463a4e6732e
NmnSach <nssachdeva2@gmail.com>
2024-04-09 03:34:34 +0530

    Added styles and modal (doesn't work yet)

--- App.js
+++ App.js
@@ -1,27 +1,86 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View, Modal } from 'react-native';
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const closeModal = () => {
+    setModalVisible(false);
+  }
+
+  const handlePress = () => {
+    console.log('Text Pressed');
+  }
   return (
+
+
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-      <Text style={styles.text}>Hello this is my first native project.</Text>
-      <StatusBar style="auto" />
+
+      <Modal style={styles.modal} visible={modalVisible}>
+        <SafeAreaView style={{ backgroundColor: 'black' }}>
+          <Text >Modal</Text>
+          <Button title="Close" onPress={closeModal} />
+        </SafeAreaView>
+      </Modal>
+
+
+
+      <View onPress={handlePress} style={styles.time1}>
+        <Text style={styles.text1}>4:00</Text>
+        {/* <Text style={styles.text1}>Hello this is my first native project.</Text> */}
+        <StatusBar style="auto" />
+      </View>
+
+      <View onPress={handlePress} style={styles.time2}>
+        <Text style={styles.text2}>4:00</Text>
+        {/* <Text style={styles.text1}>Hello this is my first native project.</Text> */}
+        <StatusBar style="auto" />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100%',
     backgroundColor: 'black', // Set background color to black
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  time1: {
+    width: '100%',
+    height: '50%',
+    backgroundColor: '#FFA500',
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: 'white', // Set text color to white
-    fontSize: 20,
-    fontWeight: 'bold',
+  time2: {
+    width: '100%',
+    height: '50%',
+    backgroundColor: '#FFF',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  text1: {
+    color: 'white',
+    fontSize: 100,
+  },
+  text2: {
+    color: 'black',
+    fontSize: 100,
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+
+
 });
 
